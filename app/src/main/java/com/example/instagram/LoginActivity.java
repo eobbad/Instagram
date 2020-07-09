2c4b761db2d4185036e60802cca90e9478722b85
eobbad <35933488+eobbad@users.noreply.github.com>
2020-07-08 21:10:06 -0700

    Created skeleton code for camera functionality

--- app/src/main/java/com/example/instagram/LoginActivity.java
+++ app/src/main/java/com/example/instagram/LoginActivity.java
@@ -28,7 +28,7 @@ public class LoginActivity extends AppCompatActivity {
 
         if (ParseUser.getCurrentUser() != null){
             goMainActivity();
-            
+
         }
 
         etUsername = findViewById(R.id.etUsername);
