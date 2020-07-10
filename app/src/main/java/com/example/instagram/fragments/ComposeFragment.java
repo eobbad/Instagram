e712149c5b8c13703863c110ae3262d9911bd4d0
eobbad <35933488+eobbad@users.noreply.github.com>
2020-07-10 13:17:19 -0700

    Finished the Posts homepage recyclerview implementation, posts now show successfully on my home screen

--- app/src/main/java/com/example/instagram/fragments/ComposeFragment.java
+++ app/src/main/java/com/example/instagram/fragments/ComposeFragment.java
@@ -167,21 +167,6 @@ public class ComposeFragment extends Fragment {
         });
     }
 
-    private void queryPosts() {
-        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
-        query.include(Post.KEY_USER);
-        query.findInBackground(new FindCallback<Post>() {
-            @Override
-            public void done(List<Post> posts, ParseException e) {
-                if(e != null){
-                    Log.e(TAG, "issue with getting posts", e);
-                    return;
-                }
-                for (Post post : posts){
-                    Log.i(TAG, "Post: " + post.getDescription()+ ", username: "+ post.getUser().getUsername());
-                }
-            }
-        });
-    }
+
 
 }
